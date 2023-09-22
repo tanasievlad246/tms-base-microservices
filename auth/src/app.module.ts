4741de35aef2858723099ff88eafb857214211b4
Tanasie Vlad <tanasie.vlad@dandsltd.com>
2023-09-22 16:13:17 +0300

    created basic entities for auth service

--- auth/src/app.module.ts
+++ auth/src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Module } from './user/.module';
+import { TenantModule } from './tenant/tenant.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [Module, TenantModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
