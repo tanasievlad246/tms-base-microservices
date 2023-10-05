aad378b44aef68f5450cbad83164cca88da9e658
Tanasie Vlad <tanasie.vlad@dandsltd.com>
2023-10-05 10:26:14 +0300

    Finished parcel service

--- orders/src/parcel/dto/create-parcel.dto.ts
+++ orders/src/parcel/dto/create-parcel.dto.ts
@@ -1,8 +1,27 @@
+import { IsNotEmpty, IsNumber, IsString, Min } from 'class-validator';
+
 export class CreateParcelDto {
+  @IsNumber()
+  @IsNotEmpty()
+  @Min(0.1)
   height: number;
+  @IsNumber()
+  @IsNotEmpty()
+  @Min(0.1)
   weight: number;
+  @IsNumber()
+  @IsNotEmpty()
+  @Min(0.1)
   length: number;
+  @IsNumber()
+  @IsNotEmpty()
+  @Min(0.1)
   width: number;
+  @IsNumber()
+  @IsNotEmpty()
+  @Min(1)
   qty: number;
+  @IsNotEmpty()
+  @IsString()
   type: string;
 }
--- orders/src/parcel/parcel.service.ts
+++ orders/src/parcel/parcel.service.ts
@@ -2,30 +2,80 @@ import { Injectable } from '@nestjs/common';
 import { CreateParcelDto } from './dto/create-parcel.dto';
 import { UpdateParcelDto } from './dto/update-parcel.dto';
 import { Parcel } from './entities/parcel.entity';
+import { DataSource, In, Repository } from 'typeorm';
+import { TenantService } from 'src/tenant/tenant.service';
 
 @Injectable()
 export class ParcelService {
-  create(createParcelDto: CreateParcelDto) {
-    return 'This action adds a new parcel';
+  constructor(
+    private readonly dataSource: DataSource,
+    private readonly tenantService: TenantService,
+  ) {}
+  async create(
+    createParcelDto: CreateParcelDto,
+    tenantId: string,
+  ): Promise<Parcel> {
+    return await this.dataSource.transaction(async (manager) => {
+      const repo: Repository<Parcel> = manager.getRepository(Parcel);
+      await this.tenantService.setCurrentTenantOnRepository(repo, tenantId);
+      const parcel: Parcel = repo.create(createParcelDto);
+      return await repo.save(parcel);
+    });
   }
 
-  findAll() {
-    return `This action returns all parcel`;
+  async findAll(tenantId: string): Promise<Parcel[]> {
+    return await this.dataSource.transaction(async (manager) => {
+      const parcelRepo: Repository<Parcel> = manager.getRepository(Parcel);
+      await this.tenantService.setCurrentTenantOnRepository(
+        parcelRepo,
+        tenantId,
+      );
+
+      return await parcelRepo.find();
+    });
   }
 
   async findManyByIds(ids: number[], tenantId: string): Promise<Parcel[]> {
-    return `This action returns all parcel`;
+    return await this.dataSource.transaction(async (manager) => {
+      const parcelRepo: Repository<Parcel> = manager.getRepository(Parcel);
+      await this.tenantService.setCurrentTenantOnRepository(
+        parcelRepo,
+        tenantId,
+      );
+
+      return await parcelRepo.findBy({ id: In(ids) });
+    });
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} parcel`;
+  async findOne(id: number, tenantId: string): Promise<Parcel> {
+    return await this.dataSource.transaction(async (manager) => {
+      const parcelRepo: Repository<Parcel> = manager.getRepository(Parcel);
+      await this.tenantService.setCurrentTenantOnRepository(
+        parcelRepo,
+        tenantId,
+      );
+
+      return await parcelRepo.findOneBy({ id });
+    });
   }
 
-  update(id: number, updateParcelDto: UpdateParcelDto) {
-    return `This action updates a #${id} parcel`;
+  async update(id: number, updateParcelDto: UpdateParcelDto): Promise<Parcel> {
+    return await this.dataSource.transaction(async (manager) => {
+      const parcelRepo: Repository<Parcel> = manager.getRepository(Parcel);
+      const parcel: Parcel = await parcelRepo.findOneBy({ id });
+      parcelRepo.merge(parcel, updateParcelDto);
+      return await parcelRepo.save(parcel);
+    });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} parcel`;
+  async remove(id: number, tenantId: string) {
+    return await this.dataSource.transaction(async (manager) => {
+      const parcelRepo: Repository<Parcel> = manager.getRepository(Parcel);
+      await this.tenantService.setCurrentTenantOnRepository(
+        parcelRepo,
+        tenantId,
+      );
+      return await parcelRepo.delete(id);
+    });
   }
 }
